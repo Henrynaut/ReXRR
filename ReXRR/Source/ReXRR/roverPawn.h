26132fddcc10dc9bb7d93fa043a101c5f7494dfd
Neil McHenry <neilmchenry@gmail.com>
2020-09-24 15:57:57 -0700

    updated roverPawn.h

--- ReXRR/Source/ReXRR/roverPawn.h
+++ ReXRR/Source/ReXRR/roverPawn.h
@@ -13,5 +13,28 @@ UCLASS()
 class REXRR_API AroverPawn : public AWheeledVehicle
 {
 	GENERATED_BODY()
+
+public:
+
+	AroverPawn();
+
+	virtual void Tick(float DeltaTime) override;
+
+	virual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+	//Throttle and Steering
+	void ApplyThrottle(float Val);
+	void ApplySteering(float Val);
+
+	//Look around
+	void LookUp(float Val);
+	void Turn(float Val);
+
+	//Handbrake
+	void OnHandbrakePressed();
+	void OnHandbrakeReleased();
+
+	//Update in air and upsidedown physics
+	void UpdateInAirControl(float DeltaTime);
 	
 };
