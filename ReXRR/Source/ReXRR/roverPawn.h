bc8e60e29830b532f8c6c1439d1a19441c9fc4bd
Neil McHenry <neilmchenry@gmail.com>
2020-09-24 17:39:38 -0500

    Created roverPawn

---
+++ ReXRR/Source/ReXRR/roverPawn.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WheeledVehicle.h"
+#include "roverPawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REXRR_API AroverPawn : public AWheeledVehicle
+{
+	GENERATED_BODY()
+	
+};
